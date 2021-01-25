2bde56a7e17179da7b1ba98e31c909818f115117
Charles Zhang <szinubuntu@gmail.com>
2021-01-24 23:32:41 -0500

    Optimization
    - Update colors configuration
    - Replace YAML reader with a custom implementation for faster reading speed

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using YamlDotNet.Serialization;
 
 namespace Converter
 {
@@ -18,7 +17,6 @@ namespace Converter
     }
     internal class Node
     {
-        public int[] Pos { get; set; }
         public string Name { get; set; }
     }
 
@@ -28,17 +26,19 @@ namespace Converter
         public int R { get; set; }
         public int G { get; set; }
         public int B { get; set; }
+        public int A { get; set; }
 
         public ColorDefinition()
         {
         }
 
-        public ColorDefinition(string name, int r, int g, int b)
+        public ColorDefinition(string name, int r, int g, int b, int a)
         {
             Name = name;
             R = r;
             G = g;
             B = b;
+            A = a;
         }
     }
 
@@ -56,28 +56,69 @@ namespace Converter
             // Extract parameters
             string sourcePath = args[0];
             string targetPath = args[1];
+            Console.WriteLine($"Processing files: \n  Input - {sourcePath} \n  Output - {targetPath}");
 
             // Start extraction
-            Region region = new Deserializer().Deserialize<Region>(File.ReadAllText(sourcePath));
+            Console.WriteLine("Reading source region...");
+            Region region = ParseRegion(File.ReadAllLines(sourcePath));
             Console.WriteLine($"{region.Nodes.Length} nodes loaded.");
 
             // Read colors
-            Dictionary<string, ColorDefinition> colors = ParseColors(ReadResource("Data.Colors.txt"));
+            Console.WriteLine("Read color definitions...");
+            string workingDirectory = Directory.GetCurrentDirectory();
+            string colorsFilePath = Path.Combine(workingDirectory, "Colors.txt");
+            Dictionary<string, ColorDefinition> presetColors;
+            if(File.Exists(colorsFilePath))
+            {
+                Console.WriteLine($"Custom color configuration file found ({colorsFilePath}), use custom colors.");
+                presetColors = ParseColors(File.ReadAllText(colorsFilePath));
+            }
+            else
+            {
+                Console.WriteLine($"No custom color configuration file is found in current working directory ({workingDirectory}), use internal colors.");
+                presetColors = ParseColors(ReadResource("Data.Colors.txt"));
+            }
 
             // Colors preprocessing
+            Console.WriteLine("Preprocessing referenced colors...");
             HashSet<string> uniqueNodes = region.Nodes.Select(n => n.Name).Distinct().ToHashSet();
-            colors = colors.Where(c => uniqueNodes.Contains(c.Key)).ToDictionary(p => p.Key, p => p.Value);
-            List<ColorDefinition> indices = colors.Values.ToList();
-            var colorIndex = indices.Select((c, i) => new KeyValuePair<string, int>(c.Name, i))
+            var referencedColors = presetColors.Where(c => uniqueNodes.Contains(c.Key)).ToDictionary(p => p.Key, p => p.Value);
+            List<ColorDefinition> colorIndices = referencedColors.Values.ToList();
+            Dictionary<string, int> colorIndexDict = colorIndices.Select((c, i) => new KeyValuePair<string, int>(c.Name, i))
                 .ToDictionary(p => p.Key, p => p.Value);
             // Exception handling
-            if(colors.Count() > 255)
+            if(referencedColors.Count() > 255)
             {
                 Console.WriteLine("[Error] More than 255 colors are used.");
                 return -2;
             }
+            // Missing color handling
+            var missingColors = uniqueNodes.Where(n => !presetColors.ContainsKey(n)).ToList();
+            if(referencedColors.Count == 0)
+            {
+                Console.WriteLine("[Warning] None of the node types are recognized in color file, please fix the following:");
+                foreach (string item in uniqueNodes)
+                    if(item != "air" && item != "ignore")
+                        Console.WriteLine($"\t{item}");
+            }
+            else if(missingColors.Count != 0)
+            {
+                Console.WriteLine($"[Warning] {missingColors.Count} colors are not fonud in color presets, please fix the following:");
+                foreach (var name in missingColors)
+                {
+                    if(name == "air")
+                        Console.WriteLine($"  Node type \"{name}\" is not defined in color file - but it's also not needed; Will treat as empty (color index 0).");
+                    else if(name == "ignore")
+                        Console.WriteLine("  Exported region contains node type \"ignore\"; Will treat as empty (color index 0).");
+                    else if (colorIndices.Count > 1)
+                        Console.WriteLine($"  Node type \"{name}\" is not defined in color file; Will use `{colorIndices[1].Name}` instead (color index 1).");
+                    else
+                        Console.WriteLine($"  Node type \"{name}\" is not defined in color file; Will use color index value 1 instead.");
+                }
+            }
 
             // Perform conversion
+            Console.WriteLine("Convert source to XRAW format...");
             using (BinaryWriter writer = new BinaryWriter(File.Open(targetPath, FileMode.Create)))
             {
                 // Header
@@ -98,25 +139,25 @@ namespace Converter
                 Random random = new Random();
                 foreach (Node node in region.Nodes)
                 {
-                    if(colorIndex.ContainsKey(node.Name))
-                        writer.Write((byte)colorIndex[node.Name]);
+                    // Special handle air
+                    if (node.Name == "air") 
+                        writer.Write((byte)0);
+                    else if(colorIndexDict.ContainsKey(node.Name))
+                        writer.Write((byte)colorIndexDict[node.Name]);
                     else
-                    {
-                        writer.Write((byte)1);
-                        Console.WriteLine($"Node type \"{node.Name}\" is not defined in color file; Used `{indices[1].Name}` instead (color index 1).");
-                    }
+                        writer.Write((byte)1); // Use index 1 for absent node types
                 }
 
                 // Color Palette Buffer
                 for (int i = 0; i < 256; i++)
                 {
                     // Real colors
-                    if (i < indices.Count)
+                    if (i < colorIndices.Count)
                     {
-                        writer.Write((byte)indices[i].R); // R
-                        writer.Write((byte)indices[i].G); // G
-                        writer.Write((byte)indices[i].B); // B
-                        writer.Write((byte)255); // A
+                        writer.Write((byte)colorIndices[i].R); // R
+                        writer.Write((byte)colorIndices[i].G); // G
+                        writer.Write((byte)colorIndices[i].B); // B
+                        writer.Write((byte)colorIndices[i].A); // A
                     }
                     // Padding
                     else
@@ -132,7 +173,7 @@ namespace Converter
                 writer.Flush();
                 writer.Close();
             }
-            Console.WriteLine("Finished.");
+            Console.WriteLine("Conversion finished.");
 
             // Return success
             return 0;
@@ -157,7 +198,46 @@ namespace Converter
             }
         }
 
-        public static Dictionary<string, ColorDefinition> ParseColors(string text)
+        /// <remarks>Use a custom implementation becauase a fully-featured YAML parser is slow</remarks>
+        private static Region ParseRegion(string[] lines)
+        {
+            Region region = new Region();
+            // Read SizeX
+            if (lines[0].StartsWith("SizeX: "))
+                region.SizeX = int.Parse(lines[0].Substring(7));
+            else
+                Console.WriteLine("The first line of source file must be \"SizeX\".");
+            // Read SizeY
+            if (lines[1].StartsWith("SizeY: "))
+                region.SizeY = int.Parse(lines[1].Substring(7));
+            else
+                Console.WriteLine("The second line of source file must be \"SizeY\".");
+            // Read SizeZ
+            if (lines[2].StartsWith("SizeZ: "))
+                region.SizeZ = int.Parse(lines[2].Substring(7));
+            else
+                Console.WriteLine("The third line of source file must be \"SizeZ\".");
+            // Skip "Nodes:" line
+            if (!lines[3].StartsWith("Nodes"))
+                Console.WriteLine("The forth line of source file must be \"Nodes\".");
+            // Count non-empty lines
+            int nonempty = lines.Count(l => !string.IsNullOrWhiteSpace(l));
+            // Read nodes
+            region.Nodes = new Node[nonempty - 4];
+            for (int i = 4; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+                else
+                {
+                    region.Nodes[i - 4] = new Node { Name = line.Substring(line.IndexOf("- Name: ") + "- Name: ".Length) };
+                }
+            }
+            return region;
+        }
+
+        private static Dictionary<string, ColorDefinition> ParseColors(string text)
         {
             Dictionary<string, ColorDefinition> colors = new Dictionary<string, ColorDefinition>();
             // Parse each line
@@ -167,27 +247,31 @@ namespace Converter
                 string line  = strReader.ReadLine();
                 // EOF
                 if (line == null) break;
+
                 // Skip comment lines
                 if (line.StartsWith('#')) continue;
+                // Skip empty lines
+                else if (string.IsNullOrWhiteSpace(line)) continue;
                 // Parse space delimited values
                 else
                 {
                     // Split
                     var values = line.Split(' ');
                     // Skip ill-formatted lines
-                    if(values.Length != 4)
+                    if (values.Length != 4 && values.Length != 5)
                     {
-                        Console.WriteLine($"Color definition line \"{line}\" is ill-formatted; Skip.");
+                        Console.WriteLine($"Color definition line \"{line}\" is ill-formatted (`NodeName R G B (A)`); Skip.");
                         continue;
                     }
-                        
+
                     // Extract
                     string name = values[0];
                     int r = int.Parse(values[1]);
                     int g = int.Parse(values[2]);
                     int b = int.Parse(values[3]);
+                    int a = values.Length == 5 ? int.Parse(values[4]) : byte.MaxValue;
                     // Save
-                    colors[values[0]] = new ColorDefinition(name, r, g, b);
+                    colors[values[0]] = new ColorDefinition(name, r, g, b, a);
                 }
             }
             return colors;
